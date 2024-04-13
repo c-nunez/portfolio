a470156e974a287650e6b4c40eb24d16a42c59bc
c-nunez <nunez_cr@outlook.com>
2024-04-12 21:51:41 -0400

    visual effect added to #name

--- public/scripts/script1.js
+++ public/scripts/script1.js
@@ -1,10 +1,34 @@
-$('#theme-mode').on('click', function() {
-    if($('html').attr('data-bs-theme') == 'light'){        
-        $('html').attr('data-bs-theme', 'dark');
-        $('#theme-mode').text('☀️');
+// $('#theme-mode').on('click', function() {
+//     if($('html').attr('data-bs-theme') == 'light'){
+//         $('html').attr('data-bs-theme', 'dark');
+//         $('#theme-mode').text('☀️');
 
-    } else if($('html').attr('data-bs-theme') == 'dark'){
-        $('html').attr('data-bs-theme', 'light');
-        $('#theme-mode').text('🌙');
-    }
-});
+//     } else if($('html').attr('data-bs-theme') == 'dark'){
+//         $('html').attr('data-bs-theme', 'light');
+//         $('#theme-mode').text('🌙');
+//     }
+// });
+
+function splitTextIntoSpans(target) {
+  let elements = document.querySelectorAll(target);
+  elements.forEach((element) => {
+    element.classList.add("split-text");
+    let text = element.innerText;
+    let splitText = text
+      .split(" ")
+      .map(function (word) {
+        let char = word
+          .split("")
+          .map((char) => {
+            return `<span class="split-char">${char}</span>`;
+          })
+          .join("");
+        return `<div class="split-word">${char}&nbsp</div>`;
+      })
+      .join("");
+
+    element.innerHTML = splitText;
+  });
+}
+
+splitTextIntoSpans("#name");
